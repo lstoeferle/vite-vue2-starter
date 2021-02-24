2663ce4a6e811c066c8a829ca61eafacd53be445
lstoeferle <48953604+lstoeferle@users.noreply.github.com>
2021-02-24 13:46:09 +0100

    Added missing type declarations and changed vue init

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import Vue from "vue";
 import App from "@/App.vue";
-import VueCompositionApi from "@vue/composition-api";
+import VueCompositionApi, { createApp, h } from "@vue/composition-api";
 
-Vue.use(VueCompositionApi)
+Vue.use(VueCompositionApi);
 
 import "windi.css";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp({
+  render: () => h(App),
+});
+
+app.mount("#app");
